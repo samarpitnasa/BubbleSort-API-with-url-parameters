2ac206d0d7d7fbd1e71392de4f053f174c53e2af
Samarpit <nasasamarpit@gmail.com>
2018-05-08 17:09:54 +0530

    thirdcommit

--- apiwithparaapp/models.py
+++ apiwithparaapp/models.py
@@ -2,11 +2,9 @@ from __future__ import unicode_literals
 from django.db import models
 #class bubblesortModel(models.Model):
 def bubblesort(a):
-    idx = 0
-    i = 0
-    smallest = 0
-    for i in range(0, len(a)):
-        smallest = min(a[i:])
-        idx = a.index(smallest)
-        a[i], a[idx] = a[idx], a[i]
+    n = len(a)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
     return a
